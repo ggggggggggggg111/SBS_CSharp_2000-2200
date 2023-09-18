ca7982aa9008e0eb36caac9aa74f5e05dc37398a
ggggggggggggg111 <95688377+ggggggggggggg111@users.noreply.github.com>
2023-09-18 22:00:41 +0900

    Attack
    qwe

---
+++ Platformer2D/Assets/02.Scripts/FSM/PlayerMachine.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class PlayerMachine : CharacterMachine
+{
+    public override float horizontal 
+    {
+        get => Input.GetAxisRaw("Horizontal");
+        set => base.horizontal = value; 
+    }
+
+    public override float vertical
+    {
+        get => Input.GetAxisRaw("Vertical");
+        set => base.vertical = value; 
+    }
+
+    private void Start()
+    {
+        Initialize(CharacterStateWorkflowsDataSheet.GetWorkflowsForPlayer(this));
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (ChangeState(State.JumpDown) == false)
+            if (ChangeState(State.Jump) == false)
+                ChangeState(State.SecondJump);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.LeftArrow))
+        {
+            ChangeState(State.WallSlide);
+        }
+        else if (current == State.WallSlide)
+        {
+            ChangeState(State.Idle);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ChangeState(State.LedgeClimb);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (canLadderUp)
+                ChangeState(State.LadderClimbing, new object[] { upLadder, DIRECTION_UP });
+            ChangeState(State.Ledge);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (canLadderDown && current == State.Idle && (upLadder != downLadder))
+                ChangeState(State.LadderClimbing, new object[] { downLadder, DIRECTION_DOWN });
+            else
+                ChangeState(State.Crouch);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (canLadderDown && current == State.Idle && (upLadder != downLadder))
+                ChangeState(State.LadderClimbing, new object[] { downLadder, DIRECTION_DOWN });
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            if (current == State.Crouch)
+                ChangeState(State.Idle);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeState(State.Attack);
+        }
+    }
+}
